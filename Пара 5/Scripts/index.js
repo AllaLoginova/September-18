f7f80421f431fdfa800c523c78b9768e11830e93
AllaLoginova <122536827+AllaLoginova@users.noreply.github.com>
2024-09-18 19:44:28 +0300

    Add files via upload

---
+++ Пара 5/Scripts/index.js
@@ -0,0 +1,28 @@
+// const obj = {
+//     name: "John",
+//     age: 19,
+//     position: "programmer",
+//     salary: 100000,
+//     married: false,
+//     backpack: ['яблоко', 200, 'кепка'],
+//     introduce() {
+//         alert(`My name is ${this.name}.`)
+//     }
+// };
+
+// obj.introduce();
+// const objToJSONStr = JSON.stringify(obj);
+// console.log(objToJSONStr);
+
+// const objFromJSONStr = JSON.parse(objToJSONStr);
+// console.log(objFromJSONStr.name);  // => John
+
+// const copyObj = JSON.parse(JSON.stringify(obj)); // копия
+// console.log(copyObj);
+// copyObj.name = 'Bob';
+// console.log(obj);
+
+// import importedObj from '../JSONs/data.json' with { type: 'json' };
+
+// console.log(importedObj);
+
